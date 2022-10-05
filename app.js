034d5f837f8194011979a4867e3781031b72ca82
Tony Wong <tywong11@gmail.com>
2022-10-05 01:42:56 -0600

    added playSingleRound

--- app.js
+++ app.js
@@ -13,3 +13,20 @@ function getComputerChoice (){
     }
     return output;
 }
+
+function playSingleRound (playerSelection, ComputerChoice){
+    //use .toLowerCase() for caps insentive
+    playerSelection = playerSelection.toLowerCase();
+    ComputerChoice = ComputerChoice.toLowerCase();
+    let output;
+    if (playerSelection == ComputerChoice) {
+        output = "It's a Tie!";
+    } else if (playerSelection == "rock" && ComputerChoice == "scissors"  &&
+               playerSelection == "scissors" && ComputerChoice == "paper" &&
+               playerSelection == "paper" && ComputerChoice == "rock" ){
+        output = "Player Wins!" + playerSelection + " beats " + ComputerChoice + ".";    
+    } else {
+        output = "Computer Wins" + ComputerChoice + " beats " + playerSelection  + ".";
+    }
+    return output;
+}
