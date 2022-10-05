af5392ab023a942f89e5560d1e0b48a3bea7a614
Tony Wong <tywong11@gmail.com>
2022-10-05 01:53:32 -0600

    sytax changes

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ function getComputerChoice (){
     // Returns 0-2 (3 options)
     let ran = Math.floor(Math.random()*3);
     let output; 
-    console.log(ran)  
     if (ran == 0){
          output = "Rock";
     } else if (ran == 1){
@@ -21,12 +20,16 @@ function playSingleRound (playerSelection, ComputerChoice){
     let output;
     if (playerSelection == ComputerChoice) {
         output = "It's a Tie!";
-    } else if (playerSelection == "rock" && ComputerChoice == "scissors"  &&
-               playerSelection == "scissors" && ComputerChoice == "paper" &&
+    } else if (playerSelection == "rock" && ComputerChoice == "scissors"  ||
+               playerSelection == "scissors" && ComputerChoice == "paper" ||
                playerSelection == "paper" && ComputerChoice == "rock" ){
-        output = "Player Wins!" + playerSelection + " beats " + ComputerChoice + ".";    
+        output = "Player Wins! " + playerSelection + " beats " + ComputerChoice + ".";    
     } else {
-        output = "Computer Wins" + ComputerChoice + " beats " + playerSelection  + ".";
+        output = "Computer Wins! " + ComputerChoice + " beats " + playerSelection  + ".";
     }
     return output;
+}
+
+function game(){
+    for
 }
